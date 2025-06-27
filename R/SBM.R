dd18730b14d0a35642111d80145ea9672fa58c15
Steven Mou <smou@eduroam-campus-10-36-3-245.campus-dynamic.uwaterloo.ca>
2025-06-26 23:01:29 -0400

    load_all for some files

--- R/SBM.R
+++ R/SBM.R
@@ -1,38 +1,36 @@
-library(igraph)
-library(Matrix)
-
+devtools::load_all()
 
 # This function generates a static Stochastic Block Model
 #  optional: generates a Degree Corrected SBM
 generate_sbm <- function(n, K, Z, B, theta = NULL, seed = NULL) {
   # n: # of nodes
   # K: # of blocks (communities)
-  # Z: Block membership matrix (n x K) 
+  # Z: Block membership matrix (n x K)
   # B: Block connection matrix (K x K)
-  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N) 
+  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N)
   # seed: (Optional) Seed for reproducibility
-  
+
   if (!is.null(seed)) set.seed(seed)
-  
+
   # Create adjacency matrix
   A <- matrix(0, nrow=n, ncol=n)
-  
+
   # Determine Edge probabilities
   for (i in 1:n) {
     for (j in i:n) {
       # Get block memberships of nodes i and j
-      i_block <- which(Z[i, ] == 1) 
+      i_block <- which(Z[i, ] == 1)
       j_block <- which(Z[j, ] == 1)
-      
+
       # Calculating edge probability using B
       prob_ij <- B[i_block, j_block]
       # Degree Correction (if given theta)
       if (!is.null(theta)) {
         prob_ij <- prob_ij * theta[i] * theta[j]
         # Prevents overflow
-        prob_ij <- min(prob_ij, 1) 
+        prob_ij <- min(prob_ij, 1)
       }
-      
+
       # Determine (in)existence of edge
       if (i != j && runif(1) < prob_ij) {
         A[i,j] <- 1
@@ -40,32 +38,32 @@ generate_sbm <- function(n, K, Z, B, theta = NULL, seed = NULL) {
       }
     }
   }
-  
+
   return(A)
 }
 
 # Generates a dynamic SB
 #  optional: generate a dynamic DCSBM
 generate_dynamic_sbm <- function(n, K, Z, B, new_B,theta=NULL, T = 10,
-                                 persistence=0.1, start_time, 
+                                 persistence=0.1, start_time,
                                  end_time=start_time, seed = NULL) {
   # n: # of nodes
   # K: # of blocks (communities)
-  # Z: Block membership matrix (n x K) 
+  # Z: Block membership matrix (n x K)
   # B: Block connection matrix (K x K)
   # new_B: New Block connection matrix (K x K)
-  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N) 
+  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N)
   # T: # of time steps for dynamic simulation
   # persistence: probability of block change for a node
   # start_time: starting timestep of the changepoint
   # end_time: last timestep of the changepoint
-  
+
   # List of adjacency matrices at each time step
   A_list <- list()
   # Starting persistence should be 0 until changepoint occurs
   cur_persistence = 0
   B_t <- B #active B
-  
+
   for (t in 1:T) {
     # Changepoint occurs, persistence changes
     if (t == start_time){
@@ -91,6 +89,6 @@ generate_dynamic_sbm <- function(n, K, Z, B, new_B,theta=NULL, T = 10,
       B_t <- B
     }
   }
-  
+
   return(A_list)
 }
--- R/SBM_DT.R
+++ R/SBM_DT.R
@@ -1,3 +1,6 @@
+devtools::load_all()
+
+
 # This function generates a static Stochastic Block Model using Datatables
 #.  to make an edgelist
 #   Note: better for larger datasets
@@ -5,30 +8,30 @@
 generate_sbm_dt <- function(n, K, Z, B, theta=NULL) {
   # n: # of nodes
   # K: # of blocks (communities)
-  # Z: Block membership matrix (n x K) 
+  # Z: Block membership matrix (n x K)
   # B: Block connection matrix (K x K)
-  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N) 
-  
+  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N)
+
   # Output: An undirected datatable
-  
+
   edgelist <- data.table(i=integer(0), j=integer(0))
-  
-  
+
+
   for (i in 1:(n-1)) {
     for (j in (i+1):n) {
-      i_block <- which(Z[i, ] == 1) 
+      i_block <- which(Z[i, ] == 1)
       j_block <- which(Z[j, ] == 1)
-      
+
       prob_ij <- B[i_block, j_block]
-      
+
       # Degree Correction
       if (!is.null(theta)) {
         prob_ij <- prob_ij * theta[i] * theta[j]
       }
-      
+
       # Cap probabilities at 1 (in case of degree correction above 1)
       prob_ij <- min(prob_ij, 1)
-      
+
       if (runif(1) < prob_ij) {
         edgelist <- rbind(edgelist, data.table(i=i,j=j))
       }
@@ -43,15 +46,15 @@ generate_dynamic_sbm_dt <- function(n, K, Z, B, theta=NULL, T = 10,
                                  persistence=0.1) {
   # n: # of nodes
   # K: # of blocks (communities)
-  # Z: Block membership matrix (n x K) 
+  # Z: Block membership matrix (n x K)
   # B: Block connection matrix (K x K)
-  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N) 
+  # theta: (Optional, if DCSBM is desired) Vector of degree parameters (length N)
   # T: # of time steps for dynamic simulation
   # persistence: probability of block change for a node
-  
+
   # List of adjacency matrices at each time step
   edgelist <- data.table(from=integer(0), to=integer(0), times=vector("list", 0))
-  
+
   for (t in 1:T) {
     for (i in 1:n) {
       if (runif(1) < persistence) {
@@ -61,22 +64,22 @@ generate_dynamic_sbm_dt <- function(n, K, Z, B, theta=NULL, T = 10,
         Z[i, new_block] <- 1
       }
     }
-    
+
     for (i in 1:(n-1)) {
       for (j in (i+1):n) {
-        i_block <- which(Z[i, ] == 1) 
+        i_block <- which(Z[i, ] == 1)
         j_block <- which(Z[j, ] == 1)
-        
+
         prob_ij <- B[i_block, j_block]
-        
+
         # Degree Correction
         if (!is.null(theta)) {
           prob_ij <- prob_ij * theta[i] * theta[j]
         }
-        
+
         # Cap probabilities at 1 (in case of degree correction above 1)
         prob_ij <- min(prob_ij, 1)
-        
+
         if (runif(1) < prob_ij) {
           # check if edge exists already
           idx <- edgelist[from==i & to==j, which=TRUE]
@@ -92,6 +95,6 @@ generate_dynamic_sbm_dt <- function(n, K, Z, B, theta=NULL, T = 10,
       }
     }
   }
-  
+
   return(edgelist)
 }
